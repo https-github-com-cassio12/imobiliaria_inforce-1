ee89be976e2e0c90eb148e242d35cbe5e90b7a54
fabiosimaodeholanda <fabio.holanda322@hotmail.com>
2021-06-24 21:15:53 -0300

    Pagina de funções javascript

---
+++ js/funcoes.js
@@ -0,0 +1,47 @@
+proxima = () => {
+    var text = document.querySelector('div.proxima_img')
+    text.innerHTML = '<img src="./imagens/slideimagem2.png" width="1350px" height="350px" >'
+   
+}
+
+exibir_texto = () => {
+    var text = document.querySelector('div.button');
+    text.innerHTML = 'Simplicidade e praticidade é a forma como trabalhamos em nosso site'
+
+}
+
+veja_mais1 = (barra_da_tijuca) => {
+    var text = document.querySelector('div.mostrar_mais1');
+    text.innerHTML = 'Valor R$750.000,00'
+    text.style.color = 'black'
+}
+
+veja_mais2 = (leblon) => {
+    var text = document.querySelector('div.mostrar_mais2');
+    text.innerHTML = 'Valor R$650.000,00'
+    text.style.color = 'black'
+}
+
+veja_mais3 = (recreio_dos_bandeirantes) => {
+    var text = document.querySelector('div.mostrar_mais3');
+    text.innerHTML = 'Valor R$600.000,00'
+    text.style.color = 'black'
+}
+
+veja_mais4 = (urca) => {
+    var text = document.querySelector('div.mostrar_mais4');
+    text.innerHTML = 'Valor R$630.000,00'
+    text.style.color = 'black'
+}
+
+veja_mais5 = (barra_da_tijuca2) => {
+    var text = document.querySelector('div.mostrar_mais5');
+    text.innerHTML = 'Valor R$550.000,00'
+    text.style.color = 'black'
+}
+
+veja_mais6 = (copacamaba) => {
+    var text = document.querySelector('div.mostrar_mais6');
+    text.innerHTML = 'Valor R$800.000,00'
+    text.style.color = 'black'
+}
